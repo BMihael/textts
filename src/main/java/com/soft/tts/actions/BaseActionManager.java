56f821acce9dcc751bbbbe4bb95149d1553942c0
Mihael <mihael.belko@windowslive.com>
2023-11-29 10:30:39 +0100

    Changed visability of methods

--- src/main/java/com/soft/tts/actions/BaseActionManager.java
+++ src/main/java/com/soft/tts/actions/BaseActionManager.java
@@ -15,21 +15,22 @@ import java.util.stream.Collectors;
  * introduces the {@code allOfFutures} method that allows the aggregation of results from a list of
  * CompletableFuture instances, providing a convenient way to manage asynchronous operations.
  *
- * The class also defines the {@code applyAction} method, which is responsible for applying a specified
- * processing action to a list of linguistic units, represented by SentenceHolder objects. This method
- * utilizes an ExecutorService for parallel execution and introduces a simulated delay specified by the
- * 'timeInSecondToSimulateLoad' parameter, allowing control over the processing time for each linguistic unit.
+ * <p>The class also defines the {@code applyAction} method, which is responsible for applying a
+ * specified processing action to a list of linguistic units, represented by SentenceHolder objects.
+ * This method utilizes an ExecutorService for parallel execution and introduces a simulated delay
+ * specified by the 'timeInSecondToSimulateLoad' parameter, allowing control over the processing
+ * time for each linguistic unit.
  *
- * Additionally, the class includes the {@code submitTasks} method, which submits processing tasks for a
- * list of linguistic units to be executed asynchronously using a specified number of threads. This method
- * manages the execution of processing tasks, collects their results, and returns a CompletableFuture
- * containing the aggregated results.
+ * <p>Additionally, the class includes the {@code submitTasks} method, which submits processing
+ * tasks for a list of linguistic units to be executed asynchronously using a specified number of
+ * threads. This method manages the execution of processing tasks, collects their results, and
+ * returns a CompletableFuture containing the aggregated results.
  *
  * @param <T> The type of result produced by the linguistic unit processing action.
  */
 public abstract class BaseActionManager<T> {
 
-  public abstract List<CompletableFuture<T>> applyAction(
+  protected abstract List<CompletableFuture<T>> applyAction(
       List<SentenceHolder> tokens, ExecutorService service, int timeInSecondToSimulateLoad);
 
   public CompletableFuture<List<T>> submitTasks(List<SentenceHolder> tokens, int numOfThreads) {
@@ -49,12 +50,12 @@ public abstract class BaseActionManager<T> {
     return result;
   }
 
-  public <T> CompletableFuture<List<T>> allOfFutures(List<CompletableFuture<T>> futuresList) {
+  protected <T> CompletableFuture<List<T>> allOfFutures(List<CompletableFuture<T>> futuresList) {
     CompletableFuture<Void> allFuturesResult =
         CompletableFuture.allOf(futuresList.toArray(new CompletableFuture[0]));
     return allFuturesResult.thenApply(
         v -> futuresList.stream().map(CompletableFuture::join).collect(Collectors.<T>toList()));
   }
 
-  public abstract void logException(String action);
+  protected abstract void logException(String action);
 }
