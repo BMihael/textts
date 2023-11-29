66f6158f5d6d5c6e6c878b4da989d7df301f25ee
Mihael <mihael.belko@windowslive.com>
2023-11-29 15:48:16 +0100

    Added class for executing tasks

--- src/main/java/com/soft/tts/TaskRunner.java
+++ src/main/java/com/soft/tts/TaskRunner.java
@@ -40,7 +40,6 @@ public class TaskRunner {
       throws ExecutionException, InterruptedException {
     long milisStart = System.currentTimeMillis();
 
-//a
     TextProvider textProvider = new TextMock();
     String text = textProvider.provideText1();
     List<SentenceHolder> tokens = SentenceUtil.generateTokens(text);
